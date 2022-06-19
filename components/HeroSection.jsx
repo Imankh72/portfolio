04463109dc6878b4567960364fa558ce9d169e28
Iman <imankhorshidi72@gmail.com>
2022-06-19 11:18:01 +0430

    Add: projects page

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
   return (
-    <div className="text-center text-gray-200 mt-14 pt-16 sm:px-8 sm:text-left hero-background h-[100vh]">
+    <div className="text-gray-200 mt-14 pt-16 pl-2 sm:text-left hero-background h-[100vh]">
       <h1 className="text-4xl mt-8 sm:text-6xl md:text-7xl font-semibold">
         <span className="block">Hi,</span>
         <span className="block">I'm Iman,</span>
---
+++ pages/projects.jsx
@@ -0,0 +1,9 @@
+const projects = () => {
+  return (
+    <div className="h-[100vh] mt-14 pt-6">
+      <h1 className="text-black">Projects</h1>
+    </div>
+  );
+};
+
+export default projects;
