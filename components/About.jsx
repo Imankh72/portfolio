e234f874d25f6b7edcb32ae37821f3e586d1bf7b
Iman <imankhorshidi72@gmail.com>
2022-06-20 12:40:28 +0430

    Fix: About component

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import { FaDownload } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="darkGradient mt-14 py-6 h-[100vh] px-6">
+    <div className="darkGradient mt-14 py-6 h-[100%] px-6">
       <h1 className="text-4xl pl-2 mb-16 sm:text-5xl ">About</h1>
       <div className="flex flex-col items-center gap-10 md:flex-row">
         <div className="rounded-full overflow-hidden w-[280px] h-[300px] ">
@@ -14,6 +15,15 @@ const About = () => {
             be a MERN stack developer.
           </p>
         </div>
+      </div>
+      <div className="flex items-center justify-center gap-4 py-4 mt-12 text-2xl ">
+        <a
+          className="flex items-center justify-center gap-4"
+          href="/static/iman-khorshidi.pdf"
+          download="iman-khorshidi.pdf"
+        >
+          <FaDownload /> My CV
+        </a>
       </div>
     </div>
   );
